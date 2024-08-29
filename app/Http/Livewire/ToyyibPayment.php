5d1f956e9880ffe430daa534ecc6c9b9e518675c
IZZERAN SHAH <izzeranshah96@gmail.com>
2024-08-29 14:38:24 +0800

    Update ToyyibPayment.php

--- app/Http/Livewire/ToyyibPayment.php
+++ app/Http/Livewire/ToyyibPayment.php
@@ -3,7 +3,8 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Auth;
+use App\Models\Bill;
 class ToyyibPayment extends Component
 {
     public function render()
@@ -12,6 +13,27 @@ class ToyyibPayment extends Component
     }
 
     public function createBill(){
+        $user = Auth::user();
+        $options = array(
+            'userSecretKey' => env('TOYYIBPAY_SECRET_KEY'),
+            'categoryCode' => env('TOYYIB_CODE'),
+            'billName' => Str::limit(Auth::user()->name,26),
+            'billDescription' => 'Subscribe By'.$user->email. \Carbon\Carbon::now(),
+            'billPriceSetting'=>1,
+            'billPayorInfo' => 1,
+            'billAmount' => $amount,
+            'billReturnUrl' => route('check.Transactions.pptem'),
+            'billCallbackUrl' => route('check.Transactions.pptem'),
+            'billExternalReferenceNo' => 'TPPM',
+            'billTo' =>  Str::limit(Auth::user()->name,26),
+            'billEmail' => $user->email,
+            'billPhone' => $user->phone_number,
+            'billSplitPayment' => 0,
+            'billSplitPaymentArgs' => '',
+            'billPaymentChannel' => 2,
+            'billContentEmail' => 'Thank you for purchasing our product!',
+            'billChargeToCustomer' => 1,
+        );
 
     }
 
