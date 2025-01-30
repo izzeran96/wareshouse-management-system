f0f2e128a32b473a7c586d61d9565cfb2da66340
izzeran96 <izzeranshah96@gmail.com>
2025-01-30 15:34:09 +0000

    Update user

--- app/Http/Livewire/User/Components/UserUpdate.php
+++ app/Http/Livewire/User/Components/UserUpdate.php
@@ -1,1 +1,13 @@
-@
+<?php
+
+namespace App\Http\Livewire\User\Components;
+
+use Livewire\Component;
+
+class UserUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.components.user-update');
+    }
+}
